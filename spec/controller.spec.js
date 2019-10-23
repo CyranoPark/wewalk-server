7411c7228093c951872c96e8268695d8d13166ea
park hanjun <park.hanjun@gmail.com>
2019-10-23 16:46:47 +0900

    add test lib and circleci config

---
+++ spec/controller.spec.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+chai.use(expect);
+
+it('user should be waiting when all users are matched', done => {
+  expect(1).to.equal(1);
+  done();
+});
